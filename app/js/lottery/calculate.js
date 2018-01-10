b831f1156b7eef99114af440839b62c613b03d12
FangFeiyue <18766131540@163.com>
2018-01-10 17:03:46 +0800

    完善计算模块

--- app/js/lottery/calculate.js
+++ app/js/lottery/calculate.js
@@ -2,7 +2,7 @@
  * @Author: FangFeiyue 
  * @Date: 2018-01-10 13:56:35 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2018-01-10 15:09:06
+ * @Last Modified time: 2018-01-10 17:00:19
  */
 class Calculate{
     /**
@@ -23,6 +23,54 @@ class Calculate{
         }
 
         return count;
+    }
+    /**
+     * 奖金范围预测
+     * @param {number} active 当前选中号码的个数 
+     * @param {string} play_name 当前的玩法标识
+     * @return {array} 奖金范围
+     */
+    computeBonus(active, play_name){
+        const play = play_name.split(''),
+              self = this;
+        let arr = new Array(play[1*1]).fill(0),
+            min,
+            max;
+        if (play[0] === 'r'){
+            let min_active = 5 - (11 - active);
+            if (min_active > 0){
+                if (min_active-play[1] >= 0){
+                    arr = new Array(min_active).fill(0);
+                    min = Calculate.combine(arr, play[1]).length;
+                }else{
+                    if (play[1] - 5 > 0 && active - play[1] >= 0){
+                        arr = new Array(active - 5).fill(0);
+                        min = Calculate.combine(arr, play[1] - 5).length ;
+                    }else{
+                        min = active-play[1] > -1 ? 1 : 0;
+                    }
+                }
+            }else{
+                min = active - play[1] > -1 ? 1 : 0;
+            }
+            
+            let max_active = Math.min(active, 5);
+            if (play[1] - 5 > 0){
+                if (active-play[1] >= 0){
+                    arr = new Array(active - 5).fill(0);
+                    max = Calculate.combine(arr, play[1] - 5).length;
+                }else{
+                    max = 0;
+                }
+            }else if (play[1] - 5 < 0){
+                arr = new Array(Math.min(active, 5)).fill(0);
+                max = Calculate.combine(arr, play[1]).length;
+            }else{
+                max = 1;
+            }
+        }
+
+        return [min, max].map(item => item * self.play_list.get(play_name).bonus);
     }
 
     /**
@@ -50,9 +98,11 @@ class Calculate{
                         let newArr = [].concat(arr);
                         newArr.splice(0, i + 1);
                         f(newArr, size - 1, newResult);
-                    }
+                    }  
                 }
             }
         })(arr, size, []);
     }
 }
+
+export default Calculate;
